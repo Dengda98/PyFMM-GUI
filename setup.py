6d37a27221773336c57cdabe0d023f6f445c720f
Dengda98 <zhudengda21@mails.ucas.ac.cn>
2024-11-13 00:09:05 +0800

    change "-" to "_"

--- setup.py
+++ setup.py
@@ -3,19 +3,24 @@ import os
 
 # 读取版本号
 def read_version():
-    version_file = os.path.join('pyfmm-gui', '_version.py')
+    version_file = os.path.join('pyfmm_gui', '_version.py')
     with open(version_file) as f:
         exec(f.read())
     return locals()['__version__']
 
 setup(
-    name='pyfmm-gui',
+    name='pyfmm_gui',
     version=read_version(),
     description='A simple GUI of PyFMM',
     author='Zhu Dengda',
     author_email='zhudengda@mail.iggcas.ac.cn',
     packages=find_packages(),
-    package_data={'pyfmm-gui': ['main.ui']},
+    package_data={'pyfmm_gui': ['main.ui']},
     include_package_data=True,
     python_requires='>=3.9',
+    entry_points={
+        'console_scripts': [
+            'pyfmm_gui=pyfmm_gui.main:main', 
+        ],
+    },
 )
